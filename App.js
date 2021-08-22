974ab557777c38965f7f9fcdc8586772a0262703
princekumr20 <66670896+princekumr20@users.noreply.github.com>
2021-08-22 17:35:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+
+
+import React, { Component } from 'react';
+
+import './index.css';
+
+
+class App extends Component {
+  state = {  }
+  render() { 
+
+      return ( 
+        <div>
+<div className="Abhi">
+<div className="container">
+                       <h1 className="a"> Best Wishes </h1>
+                       <p className="b">
+                       First of all a very</p>
+                      <p> This is a bond of love,</p><p> a bond of togetherness,</p><p> it’s a thread that binds <b/>life <p></p>and hearts.</p><p> Happy Raksha Bandhan!
+                  </p>     </div>
+                   
+
+                       <img className="img" src="https://lh3.googleusercontent.com/pw/AM-JKLViA4BgEM8ayTx39HJC0D5GPl73hhEiJR687xxfLWfFAtpU7QB0DAajuus-8Ze0QHxrw2M2jxydszNWU02543QoZ5g870MlxYuDnCifWPBhf3ScF2tmOfqbhcQFulXET3PrwmNoorF7mkjiYqnEEyxaxQ=s903-no?authuser=0" ></img>
+                       
+                       
+                       <img className="img1" src="https://lh3.googleusercontent.com/EUfDYDUN6bgMxoenHzvLxS6VI4lCo0286fVjqWpRHk6NjLHmp_bFDUInGrlwvuAu-WrMJgcoODe1VSYVXXh7OX8_r1YZtxyDpoZUeVgbg7ofSbzhn3iTmmoBqDkCUxj9ARYhW7GdtOThQvymujZcSy9TL_UFc4MDDoLI2AAintBkBd601lREuiZsSg4nQCNQKdNQGgN7fCSj9jF96uHH9mFjCcj8QwfnoScgZa0_jwTQqM1yhUVMfTCkD2Aw1ksB5Rvp9EEf5ZcwHdM98-fh1iUp9zW5lnvHk1zgycJ0DoVpvo9Bl35-IJvmY2CkzvJDO6f9xlVUyVEEAMZtGKxhvxMgIhgAcK14Xy8lFxsBJk0C8hkqM82P7ASFw5Ertu5eW6amhbvpXZBfU2Oxipoa8oQGtw7rNbPy6tC8lDGWbcWOYpBAR50ie_QyMCrrk77ptFxpf8hMjensB58HDdrV9FTrIoRtgjZZS9YsQcXjoaAxvNuV1V7KW4s9Gs7lX2n4_2lNhqjsMpxZ1c28_vUSS6bFe3BDdlAJzqWvIMp4ozj7HuxnbMtFITaUiYvUnnoyJsYSkUw5ZoBfhQL04gJ5kW7-6hE_T1VZclYL7MIYskmTtR8eehgS4XcATfMRgUy9FpNESKJ-9sXksgsrOLTk33O_bJp8P8e3LpSPEtgSARjFUvvejuwB30SPKOwlZPBmuqMv42mA3AIwLfZ70VWLhUS3nQ=s696-no?authuser=0" ></img>
+                      
+         </div>   
+  </div>
+  
+   
+       
+  
+           );
+  }
+}
+
+export default App;
